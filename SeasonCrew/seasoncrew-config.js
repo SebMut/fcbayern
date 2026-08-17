59ee2e38809d7ee8a7ee58779f408fe200ae30c6
SebMut <sebastian.mutter@outlook.com>
2026-08-17 23:19:47 +0200

    Add SeasonCrew production runtime configuration

---
+++ SeasonCrew/seasoncrew-config.js
@@ -0,0 +1,8 @@
+(()=>{
+  if(window.SeasonCrewConfig)return;
+  window.SeasonCrewConfig=Object.freeze({
+    environment:'production',
+    supabaseUrl:'https://kmhadzujovvxvpgblgkk.supabase.co',
+    publishableKey:'sb_publishable_JDcJGMDybnrOZcSRqtpzDg_6Ul0jr2Y'
+  });
+})();
