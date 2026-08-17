c6fb8364df2fbdb7c4f95d8288d55a2f75629b05
SebMut <sebastian.mutter@outlook.com>
2026-08-17 16:31:47 +0200

    Use neutral jersey icons instead of club favicons

--- SeasonCrew/schedule.js
+++ SeasonCrew/schedule.js
@@ -1,1 +1,7 @@
-export { BASE_M, D, MON } from '../FcBayern_Tom/schedule.js';
+import { BASE_M, MON } from '../FcBayern_Tom/schedule.js';
+import './club-kits.js';
+
+// SeasonCrew deliberately does not expose club logo domains.
+// The UI renders neutral two-colour jersey icons instead.
+export const D=Object.freeze({});
+export { BASE_M, MON };
